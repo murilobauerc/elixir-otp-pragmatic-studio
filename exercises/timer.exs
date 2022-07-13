0208f5b38d80fd55440b4b6003971a6994eb50d8
Murilo Bauer <murilo.bauer@sumup.com>
2022-07-13 15:28:39 -0300

    Puts code in multiple lines on spawn() function

--- exercises/timer.exs
+++ exercises/timer.exs
@@ -1,6 +1,9 @@
 defmodule Timer do
   def remind(reminder, seconds) do
-    spawn(fn -> :timer.sleep(seconds); IO.puts(reminder) end)
+    spawn(fn ->
+      :timer.sleep(seconds * 1000)
+      IO.puts(reminder)
+    end)
   end
 end
 

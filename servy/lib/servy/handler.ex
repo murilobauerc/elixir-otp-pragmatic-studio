daf20cf1ddcc269b399532fc2c9ffae8a2506397
Murilo Bauer <murilo.bauer@sumup.com>
2022-07-17 18:56:54 -0300

    Spawning snapshots of video cam processes

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -5,7 +5,10 @@ defmodule Servy.Handler do
   alias Servy.Utils.RequestHandlerSamples.Sample
   alias Servy.Conv
   alias Servy.BearController
+  alias Servy.VideoCam
+
   require Logger
+
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1, emojify: 1]
   import Servy.Parser, only: [parse: 1]
   import Servy.FileHandler, only: [handle_file: 2]
@@ -22,6 +25,22 @@ defmodule Servy.Handler do
     |> emojify()
     |> track()
     |> format_response()
+  end
+
+  def route(%Conv{method: "GET", path: "/snapshots"} = conv) do
+    parent = self() # the request-handling process
+
+    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("cam-1")}) end)
+    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("cam-2")}) end)
+    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("cam-3")}) end)
+
+    snapshot1 = receive do {:result, filename} -> filename end
+    snapshot2 = receive do {:result, filename} -> filename end
+    snapshot3 = receive do {:result, filename} -> filename end
+
+    snapshots = [snapshot1, snapshot2, snapshot3]
+
+    %{conv | status: 200, resp_body: inspect(snapshot1)}
   end
 
   def route(%Conv{method: "GET", path: "/kaboom"} = _conv) do
